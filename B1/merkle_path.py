b2232527623e1ca61602812aed9afac5871e596e
Joel Klint <joel.klint@gmail.com>
2017-11-10 15:19:50 +0100

    Finished first part of B3

---
+++ B1/merkle_path.py
@@ -0,0 +1,18 @@
+import hashlib
+
+file = open("merkle_input.txt", "r", encoding='utf-8')
+
+prev_digest = ""
+for i in enumerate(file):
+    index = i[0]
+    string = i[1].strip()
+
+    if index == 0:
+        prev_digest = string
+    else:
+        direction = string[0]
+        string = string[1:]
+        concat_string = string + prev_digest if direction == "L" else prev_digest + string
+        prev_digest = hashlib.sha1(bytearray.fromhex(concat_string)).hexdigest()
+
+print(prev_digest)

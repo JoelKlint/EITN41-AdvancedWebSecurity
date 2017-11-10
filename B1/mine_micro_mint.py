ab62a91d8677db81253ec4cf9cde29a527cd723d
Joel Klint <joel.klint@gmail.com>
2017-11-10 13:22:58 +0100

    Now running

--- B1/mine_micro_mint.py
+++ B1/mine_micro_mint.py
@@ -1,15 +1,29 @@
-from random import randint
-import random
+from random import randrange
+
+class MathStuff:
+    def mean_value(samples):
+        return sum(samples) / len(samples)
+
+    def calculate_confidence_interval(samples, constant):
+        mean_value = MathStuff.mean_value(samples)
+
+        standard_deviation_sum = 0
+        for sample in samples:
+            standard_deviation_sum += ((sample - mean_value)**2)**1/2
+        standard_deviation = standard_deviation_sum / len(samples)
+
+        tail = standard_deviation / len(samples)**1/2
+        return {
+            "lower": mean_value - constant * tail,
+            "upper": mean_value + constant * tail
+        }
 
 class Simulator:
-
-    def __init__(self, u, k, c, sim_count):
+    def __init__(self, u, k, c):
         self.u = u
         self.k = k
         self.c = c
-        self.sim_count = sim_count
         self.lamda = 3.66
-
 
     def simulate_once(self):
         bins = [0] * 2**u
@@ -17,39 +31,39 @@ class Simulator:
         iterations = 0
         while coin_count < c:
             iterations += 1
-            bin = random.randrange(len(bins))
+            bin = randrange(len(bins))
             bins[bin] += 1
             coin_count += 1 if bins[bin] == k else 0
 
         return iterations
 
-    def mean_value(self, iterations, sim_count):
-        totalt_iterations = sum(iterations)
-        return total_iterations/ self.sim_count
+    def calculate_mean_value(self, iterations):
+        self.mean_value = MathStuff.mean_value(iterations)
 
-    def confidence_interval(self, iterations, mean_value, sim_count):
-        for iteration in iterations:
-            standard_deviation_sum += ((iteration - mean_value)**2)**1/2
-        standard_deviation = standard_deviation_sum_tot / len(iterations)
-        interval = {"lower": mean_value - standard_deviation / self.sim_count**1/2),
-                    "upper": mean_value + standard_deviation / self.sim_count**1/2}
-        return interval
+    def calculate_confidence_interval(self, iterations):
+        self.confidence_interval = MathStuff.calculate_confidence_interval(iterations, self.lamda)
 
-    def start(self):
+    def print_report(self):
+        print("Mean: {}".format(self.mean_value))
+        print("Confidence interval lower: {}".format(self.confidence_interval["lower"]))
+        print("Confidence interval upper: {}".format(self.confidence_interval["upper"]))
+
+    def start(self, sim_count):
         total_iterations = 0
         iterations = []
         for _ in range(sim_count):
             iterations.append(self.simulate_once())
-        self.average_iterations = mean_value(iterations, sim_count)
-        confidence_interval = confidence_interval(self, iterations)
-        )
 
+        self.calculate_mean_value(iterations)
+        self.calculate_confidence_interval(iterations)
+
+        self.print_report()
 
 # Simulation config
-u = 16
-k = 2
-c = 1
-sim_count = 10
+u = 20
+k = 7
+c = 10000
+sim_count = 24
 
-simulator = Simulator(u, k, c, sim_count)
-simulator.start()
+simulator = Simulator(u, k, c)
+simulator.start(sim_count)
